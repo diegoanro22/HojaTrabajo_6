cbc3a64d0f1997951d8e8422f2beea5c6c36b9d3
diegoanro22 <diegorosalesv22@gmail.com>
2024-03-13 21:57:54 -0600

    creacion de archivo

--- Main.java
+++ Main.java
@@ -2,40 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Expression<Integer> validate = new Expression<>();
+        Scanner input = new Scanner(System.in);
+        Expression<Integer> checkfactory = new Expression<>();
         int exp = 0;
+        InterfaceInstruction mapImplement = null;
+        Archivo archivotxt = new Archivo("cards_desc.txt");
+
 
         System.out.println("Seleccione una opción:");
         System.out.println("1. HashMap");
         System.out.println("2. TreeMap");
         System.out.println("3. LinkedHashMap");
-        exp = scanner.nextInt();
+        exp = Integer.parseInt(input.nextLine());
 
         switch (exp) {
             case 1:
-                validate.checkExpression(exp);
-                menu();
+                mapImplement = checkfactory.checkExpression(exp);
                 break;
 
             case 2:
-                validate.checkExpression(exp);
-                menu();
+                mapImplement= checkfactory.checkExpression(exp);
                 break;
 
             case 3:
-                validate.checkExpression(exp);
-                menu();
+                mapImplement = checkfactory.checkExpression(exp);
                 break;
 
             default:
                 System.out.println("Opción no válida");
                 break;
         }
+        
+        int opcionMenu = menu();
+
+
+
     }
 
-    public static void menu() {
-        Scanner scanner = new Scanner(System.in);
+    public static int menu() {
+        Scanner input = new Scanner(System.in);
         System.out.println("Seleccione una opción:");
         System.out.println("1. Agregar una carta a la colección");
         System.out.println("2. Mostrar el tipo de una carta específica.");
@@ -43,7 +48,9 @@ public class Main {
         System.out.println("4. Mostrar el nombre, tipo y cantidad de cada carta de la colección ordenadas por tipo");
         System.out.println("5. Mostrar el nombre y tipo de todas las cartas existentes");
         System.out.println("6. Mostrar el nombre y tipo de todas las cartas existentes ordenadas por tipo");
-        int opcion = scanner.nextInt();
+        int opcion = Integer.parseInt(input.nextLine());
+        input.close();
+        return opcion;
 
     }
 }
